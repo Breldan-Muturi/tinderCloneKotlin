a9fa4a86ac29fa36d7c0540db265b70a6ed842c0
Turi-byte <breldanmuturi001@gmail.com>
2019-11-07 20:32:56 +0300

    Init

---
+++ app/src/main/java/turi/practice/tinderclone/activities/StartUpActivity.kt
@@ -0,0 +1,13 @@
+package turi.practice.tinderclone.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import turi.practice.tinderclone.R
+
+class StartUpActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start_up)
+    }
+}
